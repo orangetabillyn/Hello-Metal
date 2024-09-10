c1230a01dca89af854e5deb784cb75ddd4b641fd
orangetabillyn <121894219+orangetabillyn@users.noreply.github.com>
2024-09-09 19:59:42 -0700

    Initial Commit

---
+++ ContentView.swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContentView.swift
+//
+//  Created by Alleyn Murphy on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ContentView.swift/ContentView_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView_swiftApp.swift
+//  ContentView.swift
+//
+//  Created by Alleyn Murphy on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ContentView_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
